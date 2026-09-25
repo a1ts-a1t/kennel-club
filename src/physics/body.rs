8a858e0715f81e0e061682a67e57a1d428378a9e
a1ts-a1t <unrelatedalt@gmail.com>
2026-09-25 12:21:04 -0700

    feat: step resolution

--- src/physics/body.rs
+++ src/physics/body.rs
@@ -4,8 +4,9 @@ use crate::{
 };
 
 const DISTANCE_TOLERANCE: f64 = 1e-12;
+const MAX_PASSES: usize = 64;
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 pub struct Body {
     collider: Collider,
     velocity: Vec2,
@@ -21,38 +22,93 @@ impl Body {
         self.velocity = self.velocity + v;
     }
 
-    fn step(&self, t: f64) -> Self {
+    fn mut_step(&mut self, t: f64) {
         let delta = t * self.velocity;
-
-        Self {
-            collider: self.collider.translate(delta),
-            velocity: self.velocity,
-            inverse_mass: self.inverse_mass,
-        }
+        self.collider = self.collider.translate(delta);
     }
 }
 
 /// given an array of bodies, take one time step
 /// with them and return the resultant, decolided bodies
 /// in the same order as was given
-pub fn resolve_bodies(_bodies: &[Body]) -> Vec<Vec2> {
+/// resultant bodies will have stepped positions and the same inverse mass
+pub fn step(bodies: &[Body]) -> Result<Vec<Body>, ()> {
     // TODO: number of substeps can be dynamically solved for
-    let _k = 64; // number of substeps
-    todo!()
+    let k = 64; // number of substeps
+
+    let mut substeps: Vec<Body> = bodies.iter().cloned().collect();
+
+    for _ in 0..k {
+        for substep in substeps.iter_mut() {
+            substep.mut_step(1f64 / k as f64);
+        }
+
+        for _ in 0..(MAX_PASSES - 1) {
+            if !resolve_all_positions(&mut substeps)? {
+                break;
+            }
+        }
+
+        if resolve_all_positions(&mut substeps)? {
+            return Err(());
+        }
+
+        resolve_all_velocities(&mut substeps)?;
+    }
+
+    for _ in 0..(MAX_PASSES - 1) {
+        if !resolve_all_positions(&mut substeps)? {
+            break;
+        }
+    }
+
+    if resolve_all_positions(&mut substeps)? {
+        return Err(());
+    }
+
+    resolve_all_velocities(&mut substeps)?;
+
+    Ok(substeps)
 }
 
-fn resolve_all(_bodies: &mut [Body]) -> Result<bool, ()> {
-    todo!()
+/// given a bunch of bodies, mutate their positions such that they are not colliding
+fn resolve_all_positions(bodies: &mut [Body]) -> Result<bool, ()> {
+    let mut mutated = false;
+
+    let mut rest = bodies;
+    while let Some((first, tail)) = rest.split_first_mut() {
+        for other in tail.iter_mut() {
+            mutated |= resolve_positions(first, other)?;
+        }
+
+        rest = tail;
+    }
+
+    Ok(mutated)
+}
+
+/// given a bunch of bodies, mutate their velocities so they don't run into each other
+fn resolve_all_velocities(bodies: &mut [Body]) -> Result<(), ()> {
+    let mut rest = bodies;
+    while let Some((first, tail)) = rest.split_first_mut() {
+        for other in tail.iter_mut() {
+            resolve_velocities(first, other)?;
+        }
+
+        rest = tail;
+    }
+
+    Ok(())
 }
 
 /// given two bodies, mutate them such that they are not colliding
-/// return Ok(true) if any mutations happened and Ok(false) if no mutations happened
+/// return if the position mutated
 /// errors when two bodies colliders cannot be uncollided (ie two half planes)
-fn resolve(a: &mut Body, b: &mut Body) -> Result<bool, ()> {
-    let d = signed_distance(a.collider, b.collider).ok_or(())?;
+fn resolve_positions(a: &mut Body, b: &mut Body) -> Result<bool, ()> {
+    let d = signed_distance(&a.collider, &b.collider).ok_or(())?;
 
     // the bodies are neither inside each other or touching
-    if d > DISTANCE_TOLERANCE {
+    if d >= DISTANCE_TOLERANCE {
         return Ok(false);
     }
 
@@ -62,10 +118,10 @@ fn resolve(a: &mut Body, b: &mut Body) -> Result<bool, ()> {
     // TODO: figure out what to do when colliders don't have a normal
     // like two non-antiparallel half planes
     // or don't have a signed distance
-    let normal = normal(a.collider, b.collider).ok_or(())?;
+    let normal = normal(&a.collider, &b.collider).ok_or(())?;
 
     // we need to reposition something but both bodies have infinite mass
-    if d < DISTANCE_TOLERANCE && total_inverse_mass == 0f64 {
+    if total_inverse_mass == 0f64 {
         return Err(());
     }
 
@@ -75,25 +131,46 @@ fn resolve(a: &mut Body, b: &mut Body) -> Result<bool, ()> {
     );
 
     // push the two bodies to be at least DISTANCE_TOLERANCE apart
-    if d < DISTANCE_TOLERANCE {
-        let deficit = DISTANCE_TOLERANCE - d;
+    let deficit = DISTANCE_TOLERANCE - d;
 
-        // translate both bodies oppositely against the contact normal
-        // proportionally to their inverse masses
-        // such that the sum of how much they move is equal to the deficit
-        a.mut_translate(deficit * weight_a * normal);
-        b.mut_translate(-deficit * weight_b * normal);
+    // translate both bodies oppositely against the contact normal
+    // proportionally to their inverse masses
+    // such that the sum of how much they move is equal to the deficit
+    a.mut_translate(deficit * weight_a * normal);
+    b.mut_translate(-deficit * weight_b * normal);
+
+    Ok(true)
+}
+
+fn resolve_velocities(a: &mut Body, b: &mut Body) -> Result<(), ()> {
+    let d = signed_distance(&a.collider, &b.collider).ok_or(())?;
+
+    // the bodies aren't touching
+    if d > DISTANCE_TOLERANCE {
+        return Ok(());
     }
 
-    // the bodies also need their velocities adjusted
-    // or else they will keep running into each other
+    let (inverse_mass_a, inverse_mass_b) = (a.inverse_mass, b.inverse_mass);
+    let total_inverse_mass = inverse_mass_a + inverse_mass_b;
+
+    // TODO: figure out what to do when colliders don't have a normal
+    let normal = normal(&a.collider, &b.collider).ok_or(())?;
+
+    // we need to impel something but both bodies have infinite mass
+    if total_inverse_mass == 0f64 {
+        return Err(());
+    }
+
+    let (weight_a, weight_b) = (
+        inverse_mass_a / total_inverse_mass,
+        inverse_mass_b / total_inverse_mass,
+    );
 
     let approach_factor = Vec2::dot(a.velocity - b.velocity, normal);
     if approach_factor < 0f64 {
         a.mut_impel(-approach_factor * weight_a * normal);
         b.mut_impel(approach_factor * weight_b * normal);
-    }
+    };
 
-    // return true if a/b got mutated
-    Ok(d < DISTANCE_TOLERANCE || approach_factor < 0f64)
+    Ok(())
 }
--- src/physics/collider.rs
+++ src/physics/collider.rs
@@ -1,21 +1,26 @@
 use crate::math::Vec2;
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 pub enum Collider {
     HalfPlane { normal: Vec2, offset: f64 },
     Circle { center: Vec2, radius: f64 },
+    Union(Box<Collider>, Box<Collider>),
 }
 
 impl Collider {
     pub fn new_plane(normal: Vec2, offset: f64) -> Self {
         Collider::HalfPlane {
             normal: normal.normalized(),
-            offset,
+            offset: offset,
         }
     }
 
     pub fn new_circle(center: Vec2, radius: f64) -> Self {
         Collider::Circle { center, radius }
+    }
+
+    pub fn union(self, other: Collider) -> Self {
+        Collider::Union(Box::new(self), Box::new(other))
     }
 
     /// normal of the surface wrt a point, oriented to direction
@@ -31,6 +36,16 @@ impl Collider {
                     Vec2::new(1f64, 0f64)
                 }
             }
+            Collider::Union(a, b) => {
+                let sigma_a = a.signed_distance(p);
+                let sigma_b = b.signed_distance(p);
+
+                if sigma_a < sigma_b {
+                    a.normal(p)
+                } else {
+                    b.normal(p)
+                }
+            }
         }
     }
 
@@ -38,6 +53,7 @@ impl Collider {
         match self {
             Collider::HalfPlane { normal, offset } => offset - Vec2::dot(p, *normal),
             Collider::Circle { center, radius } => (p - *center).norm() - radius,
+            Collider::Union(a, b) => f64::min(a.signed_distance(p), b.signed_distance(p)),
         }
     }
 
@@ -51,8 +67,13 @@ impl Collider {
                 center: *center + p,
                 radius: *radius,
             },
+            Collider::Union(a, b) => a.translate(p).union(b.translate(p)),
         }
     }
+}
+
+pub fn union(a: Collider, b: Collider) -> Collider {
+    a.union(b)
 }
 
 /// inf(norm(p - q)) where
@@ -60,11 +81,15 @@ impl Collider {
 /// q satisfies b.signed_distance(q) <= 0
 /// in other words, the smallest signed distance between two
 /// colliders
-pub fn signed_distance(a: Collider, b: Collider) -> Option<f64> {
+pub fn signed_distance(a: &Collider, b: &Collider) -> Option<f64> {
     match (a, b) {
-        (Collider::Circle { center, radius }, other)
-        | (other, Collider::Circle { center, radius }) => {
-            Some(other.signed_distance(center) - radius)
+        (Collider::Union(a, b), other @ _) | (other @ _, Collider::Union(a, b)) => Some(f64::min(
+            signed_distance(a, other)?,
+            signed_distance(b, other)?,
+        )),
+        (Collider::Circle { center, radius }, other @ _)
+        | (other @ _, Collider::Circle { center, radius }) => {
+            Some(other.signed_distance(*center) - radius)
         }
         (
             Collider::HalfPlane {
@@ -79,7 +104,7 @@ pub fn signed_distance(a: Collider, b: Collider) -> Option<f64> {
             // non parallel half planes always intersect
             // if the half planes have the same normal, one is a subset of the other
             // furthermore, there is no smallest signed distance
-            if normal_a != -normal_b {
+            if *normal_a != -*normal_b {
                 return None;
             }
 
@@ -90,8 +115,28 @@ pub fn signed_distance(a: Collider, b: Collider) -> Option<f64> {
 
 /// gradient with respect to v of a.translate(v).signed_distance(b)
 /// evaluated at v = 0
-pub fn normal(a: Collider, b: Collider) -> Option<Vec2> {
+pub fn normal(a: &Collider, b: &Collider) -> Option<Vec2> {
     match (a, b) {
+        (Collider::Union(a, b), other @ _) => {
+            let sigma_a = signed_distance(a, other)?;
+            let sigma_b = signed_distance(b, other)?;
+
+            if sigma_a < sigma_b {
+                normal(a, other)
+            } else {
+                normal(b, other)
+            }
+        }
+        (other @ _, Collider::Union(a, b)) => {
+            let sigma_a = signed_distance(a, other)?;
+            let sigma_b = signed_distance(b, other)?;
+
+            if sigma_a < sigma_b {
+                normal(other, a)
+            } else {
+                normal(other, b)
+            }
+        }
         (
             Collider::HalfPlane {
                 normal: normal_a,
@@ -102,15 +147,15 @@ pub fn normal(a: Collider, b: Collider) -> Option<Vec2> {
                 offset: _,
             },
         ) => {
-            if normal_a != -normal_b {
+            if *normal_a != -*normal_b {
                 return None; // non-antinormal half planes are always intersecting
             }
 
-            Some(normal_a)
+            Some(*normal_a)
         }
         (hp @ Collider::HalfPlane { .. }, Collider::Circle { center, radius: _ }) => {
-            Some(-hp.normal(center))
+            Some(-hp.normal(*center))
         }
-        (Collider::Circle { center, radius: _ }, other) => Some(other.normal(center)),
+        (Collider::Circle { center, radius: _ }, other @ _) => Some(other.normal(*center)),
     }
 }
